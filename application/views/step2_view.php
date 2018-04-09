2bb786cf64dc16df16b7acc59de7f46041f45186
vishalverma30 <vishal.verma@codaemonsoftwares.com>
2018-04-09 20:18:00 +0530

    ui fixes v2

--- application/views/step2_view.php
+++ application/views/step2_view.php
@@ -11,31 +11,31 @@
               <label class="control-label">Requested Amount</label>
               <div class="col-xs-12 col-sm-12 radio">
                   <label <?php if(isset($this->session->userdata['userdata']['years_emt'])) { if($this->session->userdata['userdata']['years_emt']== 7) echo 'class="button status"'; else echo 'class=button';} else echo 'class=button';?>>
-                    <div class="label-text">$5,000-$10,000</div>
+                    <div class="label-text">$5,000 - $10,000</div>
                     <span class="continueIcon sprites"></span>
                   <input type="radio" value="NewCarPurchase"  onclick="step3(7)" class="ng-pristine ng-invalid ng-invalid-required"></label>
               </div>
               <div class="col-xs-12 col-sm-12 radio">
                   <label <?php if(isset($this->session->userdata['userdata']['years_emt'])) { if($this->session->userdata['userdata']['years_emt']== 6) echo 'class="button status"'; else echo 'class=button';}else echo 'class=button'; ?>>
-                    <div class="label-text">$10,000-$15,000</div>
+                    <div class="label-text">$10,000 - $15,000</div>
                     <span class="continueIcon sprites"></span>
                   <input type="radio" value="NewCarPurchase" onclick="step3(6)" class="ng-pristine ng-invalid ng-invalid-required"></label>
               </div>
               <div class="col-xs-12 col-sm-12 radio">
                   <label <?php if(isset($this->session->userdata['userdata']['years_emt'])) { if($this->session->userdata['userdata']['years_emt']== 5) echo 'class="button status"'; else echo 'class=button';} else echo 'class=button'; ?>>
-                    <div class="label-text">$15,000-$20,000</div>
+                    <div class="label-text">$15,000 - $20,000</div>
                     <span class="continueIcon sprites"></span>
                   <input type="radio" value="NewCarPurchase" onclick="step3(5)" class="ng-pristine ng-invalid ng-invalid-required"></label>
               </div>
               <div class="col-xs-12 col-sm-12 radio">
                   <label <?php if(isset($this->session->userdata['userdata']['years_emt'])) { if($this->session->userdata['userdata']['years_emt']== 4) echo 'class="button status"'; else echo 'class=button';} else echo 'class=button'; ?>>
-                    <div class="label-text">$20,000-$30,000</div>
+                    <div class="label-text">$20,000 - $30,000</div>
                     <span class="continueIcon sprites"></span>
                   <input type="radio" value="NewCarPurchase" onclick="step3(4)" class="ng-pristine ng-invalid ng-invalid-required"></label>
               </div>
                <div class="col-xs-12 col-sm-12 radio">
                   <label <?php if(isset($this->session->userdata['userdata']['years_emt'])) { if($this->session->userdata['userdata']['years_emt']== 3) echo 'class="button status"'; else echo 'class=button';}else echo 'class=button'; ?>>
-                    <div class="label-text">$30,000- $40,000 </div>
+                    <div class="label-text">$30,000 - $40,000 </div>
                     <span class="continueIcon sprites"></span>
                   <input type="radio" value="NewCarPurchase" onclick="step3(3)" class="ng-pristine ng-invalid ng-invalid-required"></label>
               </div>
--- application/views/step5_view.php
+++ application/views/step5_view.php
@@ -6,28 +6,29 @@
                     <!-- <h1>Compare live auto rates</h1> -->
                     <a href="" class="disclosure-sec">Disclosures</a>
                     <div class="clearfix"></div>
-                    <label class="control-label">Your Name</label>
-
-                    <div class="col-xs-12 col-sm-6 margbot_10">
-                        <input type="text" name="firstname" placeholder="Firstname" value="<?php if (isset($this->session->userdata['userdata']['firstname'])) echo $this->session->userdata['userdata']['firstname'] ?>" class="form-control" id="firstname" >
+                    <label class="control-label">Your Details</label>
+                  <div class="row">
+                    <div class="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 col-xs-12">
+                      <div class="col-xs-12 col-sm-6 margbot_10">
+                        <input type="text" name="firstname" placeholder="Firstname" value="<?php if (isset($this->session->userdata['userdata']['firstname'])) echo $this->session->userdata['userdata']['firstname'] ?>" class="form-control width_100" id="firstname" >
                     </div>
                     <span id="err1" style="color: red"></span>
 
                     <div class="col-xs-12 col-sm-6 margbot_10">
-                        <input type="text" name="lastname" placeholder="Lastname" value="<?php if (isset($this->session->userdata['userdata']['lastname'])) echo $this->session->userdata['userdata']['lastname'] ?>" class="form-control" id="lastname" >
+                        <input type="text" name="lastname" placeholder="Lastname" value="<?php if (isset($this->session->userdata['userdata']['lastname'])) echo $this->session->userdata['userdata']['lastname'] ?>" class="form-control width_100" id="lastname" >
                     </div>
                     <span id="err2" style="color: red"></span>
 
-                    <div class="col-xs-12 col-sm-6 margbot_10">
-                        <input type="text" name="address" placeholder="Address" value="<?php if (isset($this->session->userdata['userdata']['address'])) echo $this->session->userdata['userdata']['address'] ?>" class="form-control" id="address" >
+                    <div class="col-xs-12 col-sm-12 margbot_10">
+                        <input type="text" name="address" placeholder="Address" value="<?php if (isset($this->session->userdata['userdata']['address'])) echo $this->session->userdata['userdata']['address'] ?>" class="form-control width_100" id="address" >
                     </div>
                     <span id="err3" style="color: red"></span>
 <div class="clearfix">
 
                     </div>
                     <div class="col-xs-12 col-sm-6 margbot_10">
-                        <select class="form-control" style="width:auto;" name="state" id="state" onchange="get_city($(this).val())">
-                            <option value="">select state</option>
+                        <select class="form-control width_100" style="width:auto;" name="state" id="state" onchange="get_city($(this).val())">
+                            <option value="">Select State</option>
                             <?php
                             foreach ($state as $k => $v) {
                                 ?> 
@@ -39,8 +40,8 @@
                     </div>
                     <span id="err4" style="color: red"></span>
                     <div class="col-xs-12 col-sm-6 margbot_10">
-                        <select class="form-control" name="city" id="city" style="width:auto;">
-                            <option value="">select City</option> 
+                        <select class="form-control width_100" name="city" id="city" style="width:auto;">
+                            <option value="">Select City</option> 
                             <?php
                             foreach ($city as $k => $v) {
                                 ?> 
@@ -50,20 +51,22 @@
                         </select>               </div>
                     <span id="err5" style="color: red"></span>
                     <div class="col-xs-12 col-sm-6 margbot_10">
-                        <input type="text" name="zip" placeholder="Zip" value="<?php if (isset($this->session->userdata['userdata']['zip'])) echo $this->session->userdata['userdata']['zip'] ?>" class="form-control" id="zip" >
+                        <input type="text" name="zip" placeholder="Zip" value="<?php if (isset($this->session->userdata['userdata']['zip'])) echo $this->session->userdata['userdata']['zip'] ?>" class="form-control width_100" id="zip" >
                     </div>
                     <span id="err6" style="color: red"></span>
                     <div class="col-xs-12 col-sm-6 margbot_10">
-                        <input type="text" name="ssn" placeholder="SSN" value="<?php if (isset($this->session->userdata['userdata']['ssn'])) echo $this->session->userdata['userdata']['ssn'] ?>" class="form-control" id="ssn" >
+                        <input type="text" name="ssn" placeholder="SSN (###-##-####)" value="<?php if (isset($this->session->userdata['userdata']['ssn'])) echo $this->session->userdata['userdata']['ssn'] ?>" class="form-control width_100" id="ssn" >
                     </div>
                     <span id="err7" style="color: red"></span>
                     
-                    <div class="col-xs-12 col-sm-12 radio">
+                    <div class="col-xs-12 col-sm-12 radio margtop_30">
                         <button type="button" class="button" onclick="step6()">Continue<span class="continueIcon sprites"></span></button>
                                       <!--           <div class="col-xs-12 col-sm-12 radio"> <button type="button" onclick="back()" class="button_back"><span class="continueIcon sprites"></span>Back</button> </div>
                         -->
 
+                    </div>   
                     </div>
+                  </div>
                 </div>
             </div>
         </div>
