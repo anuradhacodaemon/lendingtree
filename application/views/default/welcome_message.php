27b4451889a03173044a0436a639efeb0c3167c1
vishalverma30 <vishal.verma@codaemonsoftwares.com>
2018-04-20 15:11:29 +0530

    new-ui-theme-changes

--- application/views/default/welcome_message.php
+++ application/views/default/welcome_message.php
@@ -5,7 +5,7 @@
     <div class="container">
         
         <div class="row">
-            <div class="col-lg-7 col-lg-offset-5 col-md-7 col-md-5 col-sm-7 col-sm-5 col-xs-12">
+            <div class="col-lg-7 col-lg-offset-5 col-md-8 col-md-offset-4 col-sm-7 col-sm-offset-5 col-xs-12">
                 <div class="offer-main clearfix">
                     <h1>How can we help you today?</h1>
                     <div class="offer-cont">
