1e4462241fb73203ff6829b90b25b03e7f614dd9
anuradhacodaemon <anuradha.chakraborti@codaemonsoftwares.com>
2018-04-24 18:47:20 +0530

    remove step1

--- application/views/refinance_step2.php
+++ application/views/refinance_step2.php
@@ -9,12 +9,12 @@
                     <label class="control-label">What Is Your Current Monthly Payment?</label>
                     
                     <span id="ex6CurrentSliderValLabel" class="top-value"> <span id="ex8SliderVal"><?php if(isset($this->session->userdata['monthly_payment'])) echo $this->session->userdata['monthly_payment'] ; else echo '5000';?></span></span>
- <div class="clearfix"></div>
+ <div class="clearfix"></div> <div class="slider-box">
                 <input id="ex8" name="monthly_payment" type="text" data-slider-min="5000" data-slider-max="50000" data-slider-step="5000" data-slider-value="<?php if(isset($this->session->userdata['monthly_payment'])) echo $this->session->userdata['monthly_payment'] ; else echo '5000';?>"/>
                 <span class="min-value">5000 &nbsp;</span>
                 <span class="max-value">&nbsp; 50000+</span>
                
-              
+              </div>
                 </div>
                   <div class="col-xs-12 col-sm-12 radio">
                 <button type="button" class="button" onclick="refinancestep2()">Continue<span class="continueIcon sprites"></span></button>
