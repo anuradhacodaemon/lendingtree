5b2e44398604f4617df84eaffcccae4c4d0229d0
anuradhacodaemon <anuradha.chakraborti@codaemonsoftwares.com>
2018-04-26 18:38:19 +0530

    pdf changes

--- application/views/default/refinance_step1.php
+++ application/views/default/refinance_step1.php
@@ -42,7 +42,19 @@
 slider.on("slide", function(sliderValue) {
 	document.getElementById("ex6SliderVal").textContent = numberWithCommas(sliderValue);
 });
-
+function numberWithCommas(x) {
+  x=String(x).toString();
+  var afterPoint = '';
+  if(x.indexOf('.') > 0)
+     afterPoint = x.substring(x.indexOf('.'),x.length);
+  x = Math.floor(x);
+  x=x.toString();
+  var lastThree = x.substring(x.length-3);
+  var otherNumbers = x.substring(0,x.length-3);
+  if(otherNumbers != '')
+      lastThree = ',' + lastThree;
+  return otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ",") + lastThree + afterPoint;
+}
 </script>
 
 
