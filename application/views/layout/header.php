679a709f9917100dad62f08ff9bbfca41489b242
vishalverma30 <vishal.verma@codaemonsoftwares.com>
2018-04-30 17:58:07 +0530

    bmtccu-master-ui-changes

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -1,14 +1,9 @@
   <div class="header-main clearfix">
       <div class="container">
         <div class="row">
-          <div class="col-lg-5 col-md-5 col-sm-6 col-xs-5">
-            <div class="logo">
-              <a href="<?php echo BASE_URL; ?>"><img src="<?php echo BASE_URL; ?>public/html/img/logo.svg" alt="Logo" title="Coastal Community community federal credit union" class="img-responsive"/></a>
-            </div>
-          </div>
-          <div class="col-lg-7 col-md-7 col-sm-6 col-xs-7">
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
             <div class="number">
-              <a href="tel:4092200612">Call us <strong>(409) 220-0612</strong></a>
+              Welcome to our new website! Please call us at <a href="tel:4092200612"><strong>(409) 220-0612</strong></a> if you have any questions.
             </div>
           </div>
         </div>
--- application/views/step4_view.php
+++ application/views/step4_view.php
@@ -1,7 +1,12 @@
 <div class="clearfix">
     <div class="" id="container">
         <div class="row">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12">
+                <div class="logo">
+                    <a href="<?php echo BASE_URL; ?>"><img src="<?php echo BASE_URL; ?>public/html/img/logo.png" alt="Logo" title="Coastal Community community federal credit union" class="img-responsive"/></a>
+                </div>
+            </div>
+            <div class="col-lg-7 col-md-7 col-sm-7 col-xs-12">
                 <div class="form-main clearfix">
                     <!-- <h1>Compare live auto rates</h1> -->
                     <a href="" class="disclosure-sec">Disclosures</a>
