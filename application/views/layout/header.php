fea611e2ef20d727640973724992b64f70c01fdc
vishalverma30 <vishal.verma@codaemonsoftwares.com>
2018-05-02 18:44:58 +0530

    hide two sections in sw-financial-front-end

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -3,7 +3,10 @@
         <div class="row">
           <div class="col-lg-5 col-md-5 col-sm-6 col-xs-5">
             <div class="logo">
-              <a href="<?php echo BASE_URL; ?>"><img src="<?php echo BASE_URL; ?>public/html/img/logo.svg" alt="Logo" title="Coastal Community community federal credit union" class="img-responsive"/></a>
+              <a href="<?php echo BASE_URL; ?>">
+                <img src="<?php echo BASE_URL; ?>public/html/img/logo.svg" alt="Logo" title="Coastal Community community federal credit union" class="img-responsive"/>
+                <img src="<?php echo BASE_URL; ?>public/html/img/tagline.svg" alt="Logo" title="Coastal Community community federal credit union" class="img-responsive margtop_5 hidden-xs"/>
+              </a>
             </div>
           </div>
           <div class="col-lg-7 col-md-7 col-sm-6 col-xs-7">
