88d564f9253fb781790d1289879f88a746cc4303
anuradhacodaemon <anuradha.chakraborti@codaemonsoftwares.com>
2018-05-03 16:03:02 +0530

    mail

--- application/controllers/Refinance.php
+++ application/controllers/Refinance.php
@@ -67,11 +67,11 @@ class Refinance extends CI_Controller {
         $this->load->view('refinance_step3');
     }
 
-    public function refinancestep4($id = 0,$current_milage=0) {
+    public function refinancestep4($id = 0, $current_milage = 0) {
         if ($id) {
             $data = array(
                 'vin' => $id,
-                'current_milage' =>$current_milage
+                'current_milage' => $current_milage
             );
 
             $this->session->set_userdata($data);
@@ -100,7 +100,7 @@ class Refinance extends CI_Controller {
 
             $this->session->set_userdata($data);
         }
-        
+
         $this->load->view('refinance_step5');
     }
 
@@ -124,7 +124,7 @@ class Refinance extends CI_Controller {
         }
         //echo '<pre>';
         // print_r($this->session->userdata());
-        
+
         $this->load->view('refinance_step6');
         //die;
     }
@@ -152,8 +152,10 @@ class Refinance extends CI_Controller {
         //$this->loan_model->add_loan($this->session->userdata['userdata']);
 
         if ($result > 0) {
-             $getPhone = $this->loan_model->get_phone();
-            $error = 'Your application has been submitted! Someone will be in touch with you shortly. If you have any questions, please call '.$getPhone[0]['phone'];
+            $getPhone = $this->loan_model->get_phone();
+            $error = 'Your application has been submitted! Someone will be in touch with you shortly. If you have any questions, please call ' . $getPhone[0]['phone'];
+            $this->mailformat($this->session->userdata['firstname'], $this->session->userdata['lastname'], $this->session->userdata['email']);
+
             $this->session->set_flashdata('item', array('message' => '<font color=red>' . $error . '</font>', 'class' => 'success'));
             $this->session->userdata['userdata'] = '';
             $this->session->userdata['currently_owe'] = '';
@@ -181,6 +183,51 @@ class Refinance extends CI_Controller {
 
             $this->load->view('refinancestep6_view');
         }
+    }
+
+    /** Please dont change the mailformat because template is coming from database * */
+    public function mailformat($firstname, $lastname, $email) {
+
+        //$this->load->library('email');
+        //$this->email->set_mailtype("html");
+        $config = Array(
+            'protocol' => 'sendmail',
+            'smtp_host' => 'Smtp.gmail.com',
+            'smtp_port' => 25,
+            'smtp_user' => 'codaemon123',
+            'smtp_pass' => 'codaemon1234',
+            'smtp_timeout' => '4',
+            'mailtype' => 'html',
+            'charset' => 'iso-8859-1'
+        );
+
+        $this->load->library('email', $config);
+
+        $this->email->set_newline("\r\n");
+        //$this->email->set_header('MIME-Version', '1.0; charset=utf-8');
+        //$this->email->set_header('Content-type', 'text/html');
+        $this->email->from(ADMINEMAIL, ADMINNAME);
+        //$this->email->from('anuradha.chakraborti@gmail.com', $this->session->userdata['userdata']['ud']);
+        $this->email->to('' . $email . '');
+        $this->email->subject("thank you for applying");
+        $emailtemplate = $this->loan_model->get_emailtemplate();
+        $token = array(
+            'firstname' => $firstname,
+            'lastname' => $lastname
+        );  // forming array to send in template
+        $pattern = '[%s]';
+        foreach ($token as $key => $val) {
+            $varMap[sprintf($pattern, $key)] = $val;
+        }
+
+        $emailContent = strtr($emailtemplate[0]['message'], $varMap);
+        $this->email->message($emailContent);
+        $emailSend = $this->email->send();
+        if ($emailSend) {
+            //echo $this->email->print_debugger();
+            return 1;
+        }
+        return 0;
     }
 
 }
