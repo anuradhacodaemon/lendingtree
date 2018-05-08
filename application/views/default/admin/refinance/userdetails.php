1f7ae32a0e7551ac12c36b115a9779b3da9e3a67
anuradhacodaemon <anuradha.chakraborti@codaemonsoftwares.com>
2018-05-08 15:45:45 +0530

    loan calculator

--- application/views/default/admin/refinance/userdetails.php
+++ application/views/default/admin/refinance/userdetails.php
@@ -88,9 +88,11 @@
                                         <?php echo $userDetails[0]['current_milage'] ?>
                                     </div>
                        
-                               
+                               <div class="col-sm-12"> <b>Birth Date: &nbsp;</b>
+                                    <?php echo date('d-m-Y',strtotime($userDetails[0]['dob'])); ?>
+                                </div>
                                <div class="col-sm-12"> <b>Address: &nbsp;</b>
-                                    <?php echo $userDetails[0]['address'] ?>
+                                    <?php echo str_replace("%20"," ",$userDetails[0]['address']) ?>
                                 </div>
                                
  <div class="col-sm-12"> <b>City: &nbsp;</b>
