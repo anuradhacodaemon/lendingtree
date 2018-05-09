8a809d795f029208ae08a35a85d549306594a9cb
anuradhacodaemon <anuradha.chakraborti@codaemonsoftwares.com>
2018-05-09 16:32:34 +0530

    footer

--- application/controllers/Refinance.php
+++ application/controllers/Refinance.php
@@ -190,17 +190,24 @@ class Refinance extends CI_Controller {
 
         //$this->load->library('email');
         //$this->email->set_mailtype("html");
-        $config = Array(
-            'protocol' => 'sendmail',
-            'smtp_host' => 'Smtp.gmail.com',
-            'smtp_port' => 25,
-            'smtp_user' => 'codaemon123',
-            'smtp_pass' => 'codaemon1234',
-            'smtp_timeout' => '4',
-            'mailtype' => 'html',
-            'charset' => 'iso-8859-1'
-        );
-
+        /** $config = Array(
+          'protocol' => 'sendmail',
+          'smtp_host' => 'Smtp.gmail.com',
+          'smtp_port' => 25,
+          'smtp_user' => 'codaemon123',
+          'smtp_pass' => 'codaemon1234',
+          'smtp_timeout' => '4',
+          'mailtype' => 'html',
+          'charset' => 'iso-8859-1'
+          );  * */
+        $config['protocol'] = 'smtp';
+        $config['smtp_host'] = 'ssl://in.mailjet.com';
+        $config['smtp_port'] = '25';
+        $config['smtp_user'] = '0cfe4bcb34b75be431f70ec4a8e2d7c0';
+        $config['smtp_pass'] = '4477c06d14710371d226cbe4d93fb993';
+        $config['charset'] = 'utf-8';
+        $config['mailtype'] = 'html';
+        $config['newline'] = "\r\n";
         $this->load->library('email', $config);
 
         $this->email->set_newline("\r\n");
