79f58726a55f1be7115f25fd7aa3462a4931478e
vishalverma30 <vishal.verma@codaemonsoftwares.com>
2018-05-09 12:54:22 +0530

    bmtccu-loan-calculator-ui-changes

--- application/views/default/loancalculator.php
+++ application/views/default/loancalculator.php
@@ -1,81 +1,76 @@
 <div class="main-section inner_height clearfix">
     <div class="container" id="container">
         <div class="row">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <div class="form-main clearfix">
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <div class="logo">
+                    <a href="<?php echo BASE_URL; ?>"><img src="<?php echo BASE_URL; ?>public/html/img/logo.png" alt="Logo" title="Coastal Community community federal credit union" class="img-responsive"/></a>
+                </div>
+            </div>
+            <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
                     <!-- <h1>Compare live auto rates</h1> -->
-                    <a href="" class="disclosure-sec">Disclosures</a>
-                    <div class="clearfix"></div>
-                    <label class="control-label">Loan Calculator</label>
-                    <div class="row">
-                        <div class="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 col-xs-12">
-                        <div class="radio-btn-sect">
-                          <div class="row">
-                            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-4 marg_20">
-                                <!--<input type="radio" name="type" /> -->
-                                <label class="radio-container">New Car
-                                  <input type="radio" checked="checked" name="type">
-                                  <span class="checkmark"></span>
-                                </label>
-
-                            </div>
-
-
-                            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-4 marg_20">
-                               <!--<input type="radio" name="type" /> -->
-                               <label class="radio-container">Used Car
-                                  <input type="radio" name="type">
-                                  <span class="checkmark"></span>
-                                </label>
-                            </div>
-                          </div>
-                        </div>
-
-                        <div class="col-xs-12 col-sm-12 marg_5 bg"> 
-                            <label class="control-label">Price of Car</label>
-                                <span id="ex6CurrentSliderValLabel" class="top-value">$ <label id="ex1SliderVal">5000 
-
-                                    </label></span>
-                                <div class="clearfix"></div>
-
-                                <div class="slider-box">
-                                    <input id="ex1"  name="currently_owe" type="text" data-slider-min="5000" data-slider-max="150000" data-slider-step="500" data-slider-value="5000"/> 
+                        <div class="form-main clearfix">
+                            <a href="" class="disclosure-sec">Disclosures</a>
+                            <div class="clearfix"></div>
+                            <label class="control-label">Loan Calculator</label>
+                            <div class="radio-btn-sect">
+                              <div class="row">
+                                <div class="col-xs-6 col-sm-6 col-md-6 col-lg-4 marg_20">
+                                    <!--<input type="radio" name="type" /> -->
+                                    <label class="radio-container">New Car
+                                      <input type="radio" checked="checked" name="type">
+                                      <span class="checkmark"></span>
+                                    </label>
 
                                 </div>
-                        </div>
-                                <div class="clearfix"></div>
-                                
+
+
+                                <div class="col-xs-6 col-sm-6 col-md-6 col-lg-4 marg_20">
+                                   <!--<input type="radio" name="type" /> -->
+                                   <label class="radio-container">Used Car
+                                      <input type="radio" name="type">
+                                      <span class="checkmark"></span>
+                                    </label>
+                                </div>
+                              </div>
+                            </div>
+                            <div class="col-xs-12 col-sm-12 marg_5 bg"> 
+                                <label class="control-label">Price of Car</label>
+                                    <span id="ex6CurrentSliderValLabel" class="top-value">$ <label id="ex1SliderVal">5000 
+
+                                        </label></span>
+                                    <div class="clearfix"></div>
+
+                                    <div class="slider-box">
+                                        <input id="ex1"  name="currently_owe" type="text" data-slider-min="5000" data-slider-max="150000" data-slider-step="500" data-slider-value="5000"/> 
+
+                                    </div>
+                            </div>
+                            <div class="clearfix"></div>
                                 <div class="col-xs-12 col-sm-12 marg_5 bg">
-                                <label class="control-label">Loan Period</label>
+                                    <label class="control-label">Loan Period</label>
                                     <span id="ex6CurrentSliderValLabel" class="top-value"> <label id="ex2SliderVal">12</label></span>
                                     <div class="clearfix"></div>
 
                                     <div class="slider-box">
-  <input id="ex2"  name="loan_period" type="text" data-slider-min="12" data-slider-max="216" data-slider-step="12" data-slider-value="12"/> 
-
+                                    <input id="ex2"  name="loan_period" type="text" data-slider-min="12" data-slider-max="216" data-slider-step="12" data-slider-value="12"/> 
                                     </div>
-
-</div>
-                                    <div class="clearfix"></div>
-
-                                    <div class="col-xs-12 col-sm-12 marg_5 bg">
+                                </div>
+                                <div class="clearfix"></div>
+                                <div class="col-xs-12 col-sm-12 marg_5 bg">
                                     <label class="control-label">Where are you Financing</label>
-                                        <div class="clearfix"></div>
-
-                                        <div class="slider-box">
- <input id="ex21" type="text"
+                                    <div class="clearfix"></div>
+                                    <div class="slider-box">
+                                        <input id="ex21" type="text"
                                                    data-provide="slider"
                                                    data-slider-ticks="[1.85, 4.15, 5.45]"
                                                    data-slider-ticks-labels='["Credit Union", "Bank", "Dealership"]'
                                                    data-slider-min="1"
                                                    data-slider-max="3"
                                                    data-slider-step="1"
-                                                   data-slider-value="3"
+                                                   data-slider-value="1"
                                                    data-slider-tooltip="hide" />                        
-
-                                        </div>
-
-</div>
+                                    </div>
+                                </div>
                                         <div class="clearfix"></div>
                                         <div class="col-xs-12 col-sm-12 marg_5 bg"> <label class="control-label">Interest Rate</label>
 
@@ -89,7 +84,7 @@
                                                        data-slider-min="1"
                                                        data-slider-max="3"
                                                        data-slider-step="1"
-                                                       data-slider-value="3"
+                                                       data-slider-value="1"
                                                        data-slider-tooltip="hide" onclick="getAmount(this.value)" />
 
                                             </div>
@@ -99,24 +94,21 @@
                                         <div class="col-xs-12 col-sm-12 marg_5 bg clearfix"> 
                                             <label class="control-label margbot_10">Estimated Monthly Payment</label>
                                             <div class="clearfix"></div>
-                                            <div class="col-xs-4 col-sm-4 col-lg-4 col-md-4 radio">
-                                                <label class="button">
+                                            <div class="col-xs-12 col-sm-4 col-lg-4 col-md-4 radio">
+                                                <label class="button_payment button font_14">
                                                     <div  class="label-text width_auto">$ <span id="monthly_pay1">0</span></div>
-                                                    <span class="continueIcon sprites"></span>
                                                     <input type="radio" value="$483" class="ng-pristine ng-invalid ng-invalid-required">
                                                 </label>
                                             </div>
-                                            <div class="col-xs-4 col-sm-4 col-lg-4 col-md-4 radio">
-                                                <label class="button">
+                                            <div class="col-xs-12 col-sm-4 col-lg-4 col-md-4 radio">
+                                                <label class="button_payment button font_14">
                                                     <div class="label-text width_auto">$<span id="monthly_pay2">0</span></div>
-                                                    <span class="continueIcon sprites"></span>
                                                     <input type="radio" value="$511" class="ng-pristine ng-invalid ng-invalid-required">
                                                 </label>
                                             </div>
-                                            <div class="col-xs-4 col-sm-4 col-lg-4 col-md-4 radio">
-                                                <label class="button">
+                                            <div class="col-xs-12 col-sm-4 col-lg-4 col-md-4 radio">
+                                                <label class="button_payment button font_14">
                                                     <div class="label-text width_auto">$<span id="monthly_pay3">0</span></div>
-                                                    <span class="continueIcon sprites"></span>
                                                     <input type="radio" value="$527" class="ng-pristine ng-invalid ng-invalid-required">
                                                 </label>
                                             </div>
@@ -129,15 +121,10 @@
 
                                         </div>   
                                     </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
+          </div>
         </div>
-    </div>
-</div>
+      </div>
     
     
             <!-- Banner ends here -->
