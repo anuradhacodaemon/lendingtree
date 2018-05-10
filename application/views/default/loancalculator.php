7c3c53bf2bc2382a85bea779bdb211643f2cc767
vishalverma30 <vishal.verma@codaemonsoftwares.com>
2018-05-10 16:38:17 +0530

    coastal-slider-changes

--- application/views/default/loancalculator.php
+++ application/views/default/loancalculator.php
@@ -59,69 +59,96 @@
                             <div class="clearfix"></div>
 
 
-                                    <div class="col-xs-12 col-sm-12 marg_5 bg">
-                                    <label class="control-label">Where are you Financing</label>
-                        
-                                        <div class="clearfix"></div>
-
-                                        <div class="slider-box">
- <input id="ex21" type="text"
-                                                   data-provide="slider"
-                                                   data-slider-ticks="[1.85, 4.15, 5.45]"
-                                                   data-slider-ticks-labels='["Credit Union", "Bank", "Dealership"]'
-                                                   data-slider-min="1"
-                                                   data-slider-max="3"
-                                                   data-slider-step="1"
-                                                   data-slider-value="1"
-                                                   data-slider-tooltip="hide" />                        
-
+                            <div class="col-xs-12 col-sm-12 marg_5 bg">
+                                <label class="control-label">Where are you Financing</label>
+                                <div class="clearfix"></div>
+                                <div class="radio-main">
+                                    <div class="line_bg"></div>
+                                    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                                        <!--<input type="radio" name="type" /> -->
+                                        <label class="radio-container">Credit Union
+                                          <input type="radio" checked="checked" name="finance">
+                                          <span class="checkmark"></span>
+                                        </label>
+                                    </div>
+                                    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                                        <!--<input type="radio" name="type" /> -->
+                                        <label class="radio-container">Bank
+                                          <input type="radio" name="finance">
+                                          <span class="checkmark"></span>
+                                        </label>
+                                    </div>
+                                    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                                        <!--<input type="radio" name="type" /> -->
+                                        <label class="radio-container">Dealership
+                                          <input type="radio" name="finance">
+                                          <span class="checkmark"></span>
+                                        </label>
+                                    </div>
                                 </div>
-
                             </div>
                             <div class="clearfix"></div>
                             <div class="col-xs-12 col-sm-12 marg_5 bg"> <label class="control-label">Interest Rate</label>
 
                                 <div class="clearfix"></div>
-
-                                <div class="slider-box">
-                                    <input id="ex22" type="text"
-                                           data-provide="slider"
-                                           name="rate"
-                                           data-slider-ticks="[0, 1,2]"
-                                           data-slider-ticks-labels='["1.85%", "4.15%", "5.45%"]'
-                                           data-slider-min="1"
-                                           data-slider-max="3"
-                                           data-slider-step="1"
-                                           data-slider-value="3"
-                                           data-slider-tooltip="hide"  />
-
-
+                            <div class="radio-main">
+                                <div class="line_bg"></div>
+                                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                                    <!--<input type="radio" name="type" /> -->
+                                    <label class="radio-container">1.85%
+                                      <input type="radio" checked="checked" name="rate">
+                                      <span class="checkmark"></span>
+                                    </label>
                                 </div>
-
+                                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                                    <!--<input type="radio" name="type" /> -->
+                                    <label class="radio-container">4.15%
+                                      <input type="radio" name="rate">
+                                      <span class="checkmark"></span>
+                                    </label>
+                                </div>
+                                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                                    <!--<input type="radio" name="type" /> -->
+                                    <label class="radio-container">5.45%
+                                      <input type="radio" name="rate">
+                                      <span class="checkmark"></span>
+                                    </label>
+                                </div>
+                            </div>
                             </div>
                             <div class="clearfix"></div>
                             <div class="col-xs-12 col-sm-12 marg_5 bg clearfix"> 
                                 <label class="control-label margbot_10">Estimated Monthly Payment</label>
                                 <div class="clearfix"></div>
                                 <div class="col-xs-4 col-sm-4 col-lg-4 col-md-4 radio">
-                                    <label class="button">
+                                    <label class="button_payment button font_14">
                                         <div  class="label-text width_auto">$ <span id="monthly_pay1">0</span></div>
                                         <input type="radio" value="$483" class="ng-pristine ng-invalid ng-invalid-required">
                                     </label>
                                 </div>
                                 <div class="col-xs-4 col-sm-4 col-lg-4 col-md-4 radio">
-                                    <label class="button">
-                                        <div class="label-text width_auto">$<span id="monthly_pay2">0</span></div>
+                                    <label class="button_payment button font_14">
+                                        <div class="label-text width_auto">$ <span id="monthly_pay2">0</span></div>
                                         <input type="radio" value="$511" class="ng-pristine ng-invalid ng-invalid-required">
                                     </label>
                                 </div>
                                 <div class="col-xs-4 col-sm-4 col-lg-4 col-md-4 radio">
-                                    <label class="button">
-                                        <div class="label-text width_auto">$<span id="monthly_pay3">0</span></div>
+                                    <label class="button_payment button font_14">
+                                        <div class="label-text width_auto">$ <span id="monthly_pay3">0</span></div>
                                         <input type="radio" value="$527" class="ng-pristine ng-invalid ng-invalid-required">
                                     </label>
                                 </div>
                                 <div class="clearfix"></div>
+                                <label class="control-label margbot_10 margtop_30">Estimated Yearly Payment</label>
+                                <div class="col-xs-4 col-sm-4 col-lg-4 col-md-4 radio">
+                                    <div  class="label-text width_auto">$ <span id="monthly_pay1">0</span></div>
+                                </div>
+                                <div class="col-xs-4 col-sm-4 col-lg-4 col-md-4 radio">
+                                    <div  class="label-text width_auto">$ <span id="monthly_pay1">0</span></div>
+                                </div>
+                                <div class="col-xs-4 col-sm-4 col-lg-4 col-md-4 radio">
+                                    <div  class="label-text width_auto">$ <span id="monthly_pay1">0</span></div>
+                                </div>
                             </div>
                             <div class="col-xs-12 col-sm-12 radio margtop_30">
                                 <button type="button" class="button" onclick="addloan()">Get Approved Now<span class="continueIcon sprites"></span></button>
@@ -228,34 +255,4 @@
 
 
                                     }
-
-
-
-                                    var slider = new Slider("#ex21", {
-                                        ticks: [1, 2, 3],
-                                        ticks_positions: [0, 50, 100],
-                                        ticks_snap_bounds: 100,
-                                        formatter: function (value) {
-
-                                            return  value;
-                                        },
-                                        ticks_tooltip: true,
-                                        step: 0.01
-                                    });
-
-                                    var slider = new Slider("#ex22", {
-                                        ticks: [1, 2, 3],
-                                        ticks_positions: [0, 50, 100],
-                                        ticks_snap_bounds: 100,
-                                        formatter: function (value) {
-
-                                            return  value;
-                                        },
-
-                                        ticks_tooltip: true,
-                                        step: 1,
-
-                                    });
-
-
 </script>
