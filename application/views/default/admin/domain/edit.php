03a44ee8ca697bf031c08137716d242b140b2bbc
anuradhacodaemon <anuradha.chakraborti@codaemonsoftwares.com>
2018-05-11 19:05:59 +0530

    loan

--- application/views/default/admin/domain/edit.php
+++ application/views/default/admin/domain/edit.php
@@ -63,7 +63,7 @@
                             <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Tag <span class="required">*</span>
                             </label>
                             <div class="col-md-6 col-sm-6 col-xs-12">
-                                <textarea id="tagline" required name="tagline" class="form-control col-md-7 col-xs-12" ><?php if(isset($domain[0]['tagline']))  echo $domain[0]['tagline']?></textarea>
+                                <textarea id="tagline"  name="tagline" class="form-control col-md-7 col-xs-12" ><?php if(isset($domain[0]['tagline']))  echo $domain[0]['tagline']?></textarea>
                             </div>
                         </div>
                         <div class="ln_solid"></div>
