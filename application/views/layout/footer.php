9c540e1dcf576091892c59c9744fbf4ae2f4e33f
anuradhacodaemon <anuradha.chakraborti@codaemonsoftwares.com>
2018-05-17 15:49:04 +0530

    loan

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -61,6 +61,10 @@ if (!empty($this->session->flashdata('item'))) {
             }, 4000);
             $(".forgot_section").hide();
 <?php } ?>
+    
+     calculate1();
+      calculate2();
+       calculate3();
     });
 
 </script>
