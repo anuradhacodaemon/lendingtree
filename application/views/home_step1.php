0e4f8ae8cfc663fdbcd8b834f09821133aeee070
vishalverma30 <vishal.verma@codaemonsoftwares.com>
2018-06-05 16:27:42 +0530

    bmtccu home loan ui done

--- application/views/home_step1.php
+++ application/views/home_step1.php
@@ -1,7 +1,7 @@
 <?php //echo '<pre>';
 //print_r($this->session->userdata());?>
-<div class="main-section inner_height clearfix" id="container1">
-    <div class="container" id="container">
+<div class="clearfix" id="container1">
+    <div class="" id="container">
         <div class="row">
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                 <div class="form-main clearfix">
