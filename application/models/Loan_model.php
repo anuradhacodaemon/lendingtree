27c1cdc4b2691daf80d969856bbb6d2710b4e2b3
Amit Jadhav <amit.jadhav@codaemonsoftwares.com>
2018-07-31 00:51:45 +0530

    Email template domain wise

--- application/models/Loan_model.php
+++ application/models/Loan_model.php
@@ -135,7 +135,9 @@ class Loan_model extends CI_Model {
     }
     
      public function get_emailtemplate() {
+        $domain = 'http://' . $_SERVER['SERVER_NAME'];
         $this->db->from(EMAILTEMPLATE . ' as email');
+        $this->db->like('email.domain_name', $domain,'both');
         $result = $this->db->get();
         return $result->result_array();
     }
