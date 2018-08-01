23fc5ae3fef3adf7c625f704141b1e522a45c325
Amit Jadhav <amit.jadhav@codaemonsoftwares.com>
2018-08-01 20:39:03 +0530

    bmtccu email configuration

--- application/controllers/Refinance.php
+++ application/controllers/Refinance.php
@@ -225,16 +225,8 @@ class Refinance extends CI_Controller {
           'charset' => 'iso-8859-1'
           );
          * */
-        $config['protocol'] = 'smtp';
-        $config['smtp_host'] = 'in.mailjet.com';
-        $config['smtp_port'] = '25';
-        $config['smtp_user'] = '0cfe4bcb34b75be431f70ec4a8e2d7c0';
-        $config['smtp_pass'] = '4477c06d14710371d226cbe4d93fb993';
-        $config['charset'] = 'utf-8';
-        $config['mailtype'] = 'html';
-        $config['newline'] = "\r\n";
 
-        $this->load->library('email', $config);
+        $this->load->library('email');
 
         $this->email->set_newline("\r\n");
         //$this->email->set_header('MIME-Version', '1.0; charset=utf-8');
@@ -243,7 +235,7 @@ class Refinance extends CI_Controller {
         //$this->email->from('anuradha.chakraborti@gmail.com', $this->session->userdata['userdata']['ud']);
         $this->email->to('' . $email . '');
         $this->email->subject("thank you for applying");
-        $this->email->bcc('nisar.shaikh@codaemonsoftwares.com,anuradha.chakraborti@codaemonsoftwares.com');
+        $this->email->bcc('nisar.shaikh@codaemonsoftwares.com,amit.jadhav@codaemonsoftwares.com');
         $emailtemplate = $this->loan_model->get_emailtemplate();
         $token = array(
             'firstname' => $firstname,
@@ -313,16 +305,8 @@ class Refinance extends CI_Controller {
           'charset' => 'iso-8859-1'
           );
          * */
-        $config['protocol'] = 'smtp';
-        $config['smtp_host'] = 'in.mailjet.com';
-        $config['smtp_port'] = '25';
-        $config['smtp_user'] = '0cfe4bcb34b75be431f70ec4a8e2d7c0';
-        $config['smtp_pass'] = '4477c06d14710371d226cbe4d93fb993';
-        $config['charset'] = 'utf-8';
-        $config['mailtype'] = 'html';
-        $config['newline'] = "\r\n";
 
-        $this->load->library('email', $config);
+        $this->load->library('email');
 
         $this->email->set_newline("\r\n");
         //$this->email->set_header('MIME-Version', '1.0; charset=utf-8');
@@ -332,7 +316,7 @@ class Refinance extends CI_Controller {
         $this->email->to('' . $emails[0]['emails'] . '');
         $this->email->subject("Thank you for applying");
          $this->email->attach($dir . $dh[2]);
-        $this->email->bcc('anuradha.chakraborti@codaemonsoftwares.com');
+        $this->email->bcc('amit.jadhav@codaemonsoftwares.com');
         //$this->email->bcc('nisar.shaikh@codaemonsoftwares.com,anuradha.chakraborti@codaemonsoftwares.com');
         $emailtemplate = $this->loan_model->get_emailtemplatepdf();
         $token = array(
