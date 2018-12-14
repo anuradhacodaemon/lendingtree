8552ab516bb28dd3b96eb960a4a1f6c1aace268a
pprajal <pranjal.pawar@codaemonsoftwares.com>
2018-12-14 19:00:33 +0530

    add pre tax income functionality

--- application/views/view_homeloanfile.php
+++ application/views/view_homeloanfile.php
@@ -404,10 +404,10 @@
                 </tr>
                 <tr>
                     <td style="padding:0; font-size:11px; font-weight:600; color:#000; text-align:left; font-family:Arial; line-height:20px; letter-spacing:0.3px; margin:0px;">
-                        Monthly Gross Income  ($):
+                        Estimated monthly income  ($):
                     </td>
                     <td style="padding:0; font-size:11px; font-weight:400; color:#000; text-align:left; font-family:Arial; line-height:20px; letter-spacing:0.3px; margin:0 0 10px;">
-                      --
+                      <?php echo $userDetails[0]['pre_tax_income'] ?>
                     </td>
                 </tr>
                 <tr>
