6ab0cb20a81d84f6e4ef11eb3b6bffd7a222277d
pprajal <pranjal.pawar@codaemonsoftwares.com>
2018-12-17 19:05:47 +0530

    google analytics

--- application/views/default/welcome_message.php
+++ application/views/default/welcome_message.php
@@ -1,15 +1,16 @@
-
-
 <!-- Banner starts here -->
 <div class="banner-main clearfix">
-    <div class="container">      
+    <div class="container">
         <div class="row">
-          <div class="col-lg-5 col-lg-offset-7 col-md-5 col-md-offset-7 col-sm-7 col-sm-offset-5 col-xs-12">
-                <div class="offer-main clearfix">
+            <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-12">
+                <div class="banner-inner">
                     <h1>How can we help you today?</h1>
-                    <div class="offer-cont">
-                        <div class="col-md-6 col-sm-6 col-xs-12">
-                            <a href="<?php echo BASE_URL . 'auto?step=1' ?>">
+                </div>
+                <div class="offer-main clearfix">
+                  <div class="row">
+                    <div class="col-md-4 col-sm-4 col-xs-12">
+                        <div class="offer-cont">
+                            <a href="<?php echo BASE_URL . 'auto?step=1' ?>" onClick="ga('send', 'event', 'culoanportal', 'click', 'auto loan');" >
                                 <button class="offer-circ">
                                     <div class="outer-circ">
                                         <div class="inner-bevel">
@@ -23,9 +24,9 @@
                             </a>
                         </div>
                     </div>
-                    <div class="offer-cont">
-                        <div class="col-md-6 col-sm-6 col-xs-12">
-                            <a href="<?php echo BASE_URL . 'refinance?refinancestep=1' ?>">
+                    <div class="col-md-4 col-sm-4 col-xs-12">
+                        <div class="offer-cont">
+                            <a href="<?php echo BASE_URL . 'refinance?refinancestep=1' ?>" onClick="ga('send', 'event', 'culoanportal', 'click', 'auto refinance');" >
                                 <button class="offer-circ">
                                     <div class="outer-circ">
                                         <div class="inner-bevel">
@@ -39,22 +40,23 @@
                             </a>
                         </div>
                     </div>
-					<div class="offer-cont">
-						<div class="col-md-4 col-sm-6 col-xs-12">
-							<a href="<?php echo BASE_URL . 'homeloan?homeloan=1' ?>">
-								<button class="offer-circ">
-									<div class="outer-circ">
-										<div class="inner-bevel">
-											<div class="second-bevel">
-												<div class="offer-img offer-home-loan"></div>
-												<p class="title">HOME LOANS</p>
-											</div>
-										</div>
-									</div>
-								</button>
-							</a>
-						</div>
-					</div>
+                    <div class="col-md-4 col-sm-4 col-xs-12">
+                        <div class="offer-cont">
+                            <a href="<?php echo BASE_URL . 'homeloan?homeloan=1' ?>" onClick="ga('send', 'event', 'culoanportal', 'click', 'home loan');" >
+                                <button class="offer-circ">
+                                    <div class="outer-circ">
+                                        <div class="inner-bevel">
+                                            <div class="second-bevel">
+                                                <div class="offer-img offer-home-loan"></div>
+                                                <p class="title">HOME LOANS</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </button>
+                            </a>
+                        </div>
+                    </div>
+                  </div>   
                 </div>
             </div>
             <div class="disclosure">
@@ -75,7 +77,7 @@
             </div>
             <div class="col-md-3 col-md-offset-3 col-sm-4 col-sm-offset-2 col-xs-12">
                 <div class="offer-cont">
-                    <a href="<?php echo BASE_URL . 'loancalculator' ?>">
+                    <a href="<?php echo BASE_URL . 'loancalculator' ?>" onClick="ga('send', 'event', 'culoanportal', 'click', 'loan calculator');" >
                         <button class="offer-circ">
                             <div class="outer-circ">
                                 <div class="inner-bevel">
@@ -91,7 +93,7 @@
             </div>
 			<div class="col-md-3 col-sm-4 col-xs-12">
                 <div class="offer-cont">
-                    <a href="<?php echo BASE_URL . 'mortgagecalculator' ?>">
+                    <a href="<?php echo BASE_URL . 'mortgagecalculator' ?>" onClick="ga('send', 'event', 'culoanportal', 'click', 'mortgage calculator');" >
                         <button class="offer-circ">
                             <div class="outer-circ">
                                 <div class="inner-bevel">
