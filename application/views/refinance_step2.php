bd8dbcc8331067b990c69352a8d20a296493e7f4
Haroon <haroon.m@codaemonsoftwares.com>
2023-05-20 00:07:23 +0530

    refinance step 2 changes

--- application/views/refinance_step2.php
+++ application/views/refinance_step2.php
@@ -1,12 +1,7 @@
 <div class="main-section inner_height clearfix" id="container1">
     <div class="container" id="containerrefinance">
         <div class="row">
-            <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12">
-                <div class="logo">
-                    <a href="<?php echo BASE_URL; ?>"><img src="<?php echo BASE_URL; ?>public/html/img/logo.png" alt="Logo" title="Coastal Community community federal credit union" class="img-responsive"/></a>
-                </div>
-            </div>
-            <div class="col-lg-7 col-md-7 col-sm-7 col-xs-12">
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                 <div class="form-main clearfix">
                     <h1>Refinance Your Vehicle</h1>
                     <a href="" class="disclosure-sec">Disclosures</a>
