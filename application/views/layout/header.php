5d8768049be608ec8967ac844dac6a8e42dd73f7
nisarshaikh15 <nisar.shaikh@codaemonsoftwares.com>
2023-06-22 19:57:24 +0530

    logo change and logo

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -2,13 +2,9 @@
   ?>
 <div class="header-main clearfix">
       <div class="container">
-        <div class="row">
-          <div class="col-lg-5 col-md-5 col-sm-6 col-xs-7">
             <div class="logo">
               <a href="<?php echo BASE_URL; ?>"><img src="<?php echo BASE_URL; ?>public/html/img/JCTTX_Logo_150x150px_Transp.png" alt="Logo" title="Coastal Community community federal credit union" class="img-responsive"/></a>
             </div>
-          </div>
-          <div class="col-lg-7 col-md-7 col-sm-6 col-xs-5">
             <div class="number">
                 <?php if(!empty($getPhone[0]['phone'])) { ?>
               <a href="tel:<?php echo $getPhone[0]['phone']?>">Call Us <strong><?php echo $getPhone[0]['phone']?></strong></a>
@@ -16,8 +12,6 @@
             
                 ?>
             </div>
-          </div>
-        </div>
       </div>
     </div>
     <!-- Header ends here -->
