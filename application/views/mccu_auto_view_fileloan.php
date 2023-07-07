4e6b904e35e51eaa11ef71e302d6d42cee841b21
root <root@ip-172-31-14-125.us-east-2.compute.internal>
2023-07-07 09:49:04 +0000

    client_changes_07_07_23

--- application/views/mccu_auto_view_fileloan.php
+++ application/views/mccu_auto_view_fileloan.php
@@ -265,6 +265,14 @@ function isJson($string) {
                        <?php echo $userDetails['buying_own_rent']; ?>
                     </td>
                 </tr>
+	         <tr>
+                    <td style="padding:0; font-size:11px; font-weight:600; color:#000; text-align:left; font-family:Arial; line-height:20px; letter-spacing:0.3px; mar>
+                        Monthly Payment : ($) :
+                    </td>
+                    <td style="padding:0; font-size:11px; font-weight:400; color:#000; text-align:left; font-family:Arial; line-height:20px; letter-spacing:0.3px; mar>
+                        <?php echo $userDetails['monthly_pay'] ?>
+                    </td>
+                </tr>      
                 <tr>
                     <td style="padding:0; font-size:11px; font-weight:600; color:#000; text-align:left; font-family:Arial; line-height:20px; letter-spacing:0.3px; margin:0px;">
                         Military Involvement :
@@ -619,14 +627,6 @@ function isJson($string) {
                     </td>
                     <td style="padding:0; font-size:11px; font-weight:400; color:#000; text-align:left; font-family:Arial; line-height:20px; letter-spacing:0.3px; margin:0 0 10px;">
                         <?php echo $userDetails['how_long_your_working'] ?>
-                    </td>
-                </tr>
-                <tr>
-                    <td style="padding:0; font-size:11px; font-weight:600; color:#000; text-align:left; font-family:Arial; line-height:20px; letter-spacing:0.3px; margin:0px;">
-                        Monthly Outstandings ($) :
-                    </td>
-                    <td style="padding:0; font-size:11px; font-weight:400; color:#000; text-align:left; font-family:Arial; line-height:20px; letter-spacing:0.3px; margin:0 0 10px;">
-                        <?php echo $userDetails['monthly_pay'] ?>
                     </td>
                 </tr>
                 <tr>
