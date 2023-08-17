1ead6a9fa72468e505b711692b515d4d7beeaabf
nisarshaikh15 <nisar.shaikh@codaemonsoftwares.com>
2023-08-17 15:27:18 +0530

    pdf changes

--- application/views/mccu_auto_view_fileloan.php
+++ application/views/mccu_auto_view_fileloan.php
@@ -1719,14 +1719,14 @@ function isJson($string) {
                         <?php echo date('d-m-Y' ,strtotime($userDetails['date_of_application']))?>
                     </td>
                 </tr>
-                <tr>
-                    <td style="padding:0; font-size:11px; font-weight:600; color:#000; text-align:left; font-family:Arial; line-height:20px; letter-spacing:0.3px; margin:0px;">
-                        It is a federal crime to Willfully and diliberately provide incomplete or incorrect information on loan applications made to federal credit unions or State Chartered Credit Unions insured by NCUA
-                    </td>
-                </tr>
             </table>
         </td>
     </tr>
+    <tr>
+        <td style="padding:0; font-size:11px; font-weight:600; color:#000; text-align:left; font-family:Arial; line-height:20px; letter-spacing:0.3px; margin:0px;">
+            It is a federal crime to Willfully and diliberately provide incomplete or incorrect information on loan applications made to federal credit unions or State Chartered Credit Unions insured by NCUA
+        </td>
+   </tr>
     <tr>
         <td>
             <table>
