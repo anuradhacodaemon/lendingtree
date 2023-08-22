92c09a9c6ae29d0295681dfe120d5a44515fc3e2
saurabh1390 <saurab.c@codaemonsoftwares.com>
2023-08-22 22:46:35 +0530

    monthly change

--- application/views/mccu_auto_view_fileloan.php
+++ application/views/mccu_auto_view_fileloan.php
@@ -1080,7 +1080,7 @@ function isJson($string) {
                     </tr>
                     <tr>
                         <td style="padding:0; font-size:11px; font-weight:600; color:#000; text-align:left; font-family:Arial; line-height:20px; letter-spacing:0.3px; margin:0px;">
-                        Monthly Outstandings ($) :
+                        Monthly Payment ($) :
                         </td>
                         <td style="padding:0; font-size:11px; font-weight:400; color:#000; text-align:left; font-family:Arial; line-height:20px; letter-spacing:0.3px; margin:0 0 10px;">
                             <?php echo $userDetails['cosigner_monthly_pay'];  ?>
