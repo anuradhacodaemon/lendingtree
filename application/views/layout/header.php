16a944f7ffb42911d5dd60bc72ec5d07cbdd1f5c
root <root@ip-172-31-14-125.us-east-2.compute.internal>
2023-09-11 07:37:10 +0000

    cache

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -4,7 +4,7 @@
         <div class="row">
           <div class="col-lg-5 col-md-5 col-sm-6 col-xs-7">
             <div class="logo">
-              <a href="<?php echo BASE_URL; ?>"><img src="<?php echo BASE_URL; ?>public/html/img/Image-3.png" alt="Logo" title="Coastal Community community federal credit union" class="img-responsive"/></a>
+              <a href="<?php echo BASE_URL; ?>"><img src="<?php echo BASE_URL; ?>public/html/img/BEYOU-logo.png" alt="Logo" title="Coastal Community community federal credit union" class="img-responsive"/></a>
             </div>
           </div>
           <div class="col-lg-7 col-md-7 col-sm-6 col-xs-5">
