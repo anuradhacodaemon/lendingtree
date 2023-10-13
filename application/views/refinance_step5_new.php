48fb233870a09eba5f50d113caceb36c6492d334
nisarshaikh15 <nisar.shaikh@codaemon.com>
2023-10-13 15:35:35 +0530

    issue fixes

--- application/views/refinance_step5_new.php
+++ application/views/refinance_step5_new.php
@@ -7,7 +7,12 @@
   <div class="clearfix">
       <div class="container" id="container">
         <div class="row">
-          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+          <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12">
+                <div class="logo">
+                    <a href="<?php echo BASE_URL; ?>"><img src="<?php echo BASE_URL; ?>public/html/img/logo.png" alt="Logo" title="Beaumont Community community federal credit union" class="img-responsive"/></a>
+                </div>
+            </div>
+            <div class="col-lg-7 col-md-7 col-sm-7 col-xs-12">
             <div class="form-main clearfix">
               <h1>Refinance Your Vehicle</h1>
               <!--<a href="" class="disclosure-sec">Disclosures</a>-->
