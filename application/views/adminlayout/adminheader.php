0575c159e62f1307ffdf51f965d3910593e7b124
roshanimalik09 <roshani.m@codaemonsoftwares.com>
2023-10-30 18:05:12 +0530

    Adminside personal loan changes

--- application/views/adminlayout/adminheader.php
+++ application/views/adminlayout/adminheader.php
@@ -1,70 +1,70 @@
+<div class="col-md-3 left_col">
+    <div class="left_col scroll-view">
+
+        <div class="navbar nav_title" style="border: 0;">
+            <a href="<?php echo BASE_URL . 'admin/user' ?>" class="site_title"><img
+                    src="<?php echo BASE_URL ?>public/admin/images/gray_logo_yellow_background_user.png"><span>Culoanportal</span></a>
+        </div>
+        <div class="clearfix"></div>
 
 
-                <div class="col-md-3 left_col">
-                    <div class="left_col scroll-view">
-
-                        <div class="navbar nav_title" style="border: 0;">
-                            <a href="<?php echo BASE_URL.'admin/user' ?>" class="site_title"><img src="<?php echo BASE_URL ?>public/admin/images/gray_logo_yellow_background_user.png"><span>Culoanportal</span></a>
-                        </div>
-                        <div class="clearfix"></div>
-
-
-                        <!-- menu prile quick info -->
-                        <div class="profile col-lg-12">
-                            <!--<div class="profile_pic">
+        <!-- menu prile quick info -->
+        <div class="profile col-lg-12">
+            <!--<div class="profile_pic">
                                 <img src="<?php echo BASE_URL ?>public/images/img.jpg" alt="..." class="img-circle profile_img">
                             </div>-->
-                            <div class="profile_info">
-                                <span>Welcome</span>
-                            </div>
-                        </div>
-                        <!-- /menu prile quick info -->
+            <div class="profile_info">
+                <span>Welcome</span>
+            </div>
+        </div>
+        <!-- /menu prile quick info -->
 
-                        <br />
+        <br />
 
-                        <!-- sidebar menu -->
-                        <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+        <!-- sidebar menu -->
+        <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
 
-                            <div class="menu_section">
-                                <h3>&nbsp; </h3>
-                                <ul class="nav side-menu">
-                                       <li><a href="<?php echo BASE_URL ?>admin/dashboard"><i class="fa fa-edit"></i>Dashboard</a>
-                                    </li>
-                                    <li><a href="<?php echo BASE_URL ?>admin/user"><i class="fa fa-edit"></i>Auto Loan User</a>
-                                    </li>
-                                    
-                                     <li><a href="<?php echo BASE_URL ?>admin/refinance"><i class="fa fa-edit"></i>Auto Refinance User</a>
-                                    </li>
-                                    
-                                     <li><a href="<?php echo BASE_URL ?>admin/homeloan"><i class="fa fa-edit"></i>Home Loan</a>
-                                    </li>
-                                     <li><a href="<?php echo BASE_URL ?>admin/domain"><i class="fa fa-edit"></i>Settings</a>
-                                    </li>
-                                    
-                                    
-                                    <li><a href="<?php echo BASE_URL ?>admin/template"><i class="fa fa-edit"></i>Template for lead</a>
-                                    </li>
-                                     <li><a href="<?php echo BASE_URL ?>admin/template2/edit/0"><i class="fa fa-edit"></i>Template2 for admin</a>
-                                    </li> 
-                                   
+            <div class="menu_section">
+                <h3>&nbsp; </h3>
+                <ul class="nav side-menu">
+                    <li><a href="<?php echo BASE_URL ?>admin/dashboard"><i class="fa fa-edit"></i>Dashboard</a>
+                    </li>
+                    <li><a href="<?php echo BASE_URL ?>admin/user"><i class="fa fa-edit"></i>Auto Loan User</a>
+                    </li>
+                    <li><a href="<?php echo BASE_URL ?>admin/personalloanmccu"><i class="fa fa-bank"></i>Personal
+                            Loan</a></li>
+                    <li><a href="<?php echo BASE_URL ?>admin/refinance"><i class="fa fa-edit"></i>Auto Refinance
+                            User</a>
+                    </li>
 
-					<li><a href="<?php echo BASE_URL ?>admin/home/logout"><i class="fa fa-power-off">   </i> Logout</a>
-                    </li>		
-                    
-                    
-                                </ul>
-                                </li>
-                                </ul>
-                            </div>
+                    <li><a href="<?php echo BASE_URL ?>admin/homeloan"><i class="fa fa-edit"></i>Home Loan</a>
+                    </li>
+                    <li><a href="<?php echo BASE_URL ?>admin/domain"><i class="fa fa-edit"></i>Settings</a>
+                    </li>
 
-                        </div>
-                        <!-- /sidebar menu -->
 
-                        <!-- /menu footer buttons -->
+                    <li><a href="<?php echo BASE_URL ?>admin/template"><i class="fa fa-edit"></i>Template for lead</a>
+                    </li>
+                    <li><a href="<?php echo BASE_URL ?>admin/template2/edit/0"><i class="fa fa-edit"></i>Template2 for
+                            admin</a>
+                    </li>
 
-                        <!-- /menu footer buttons -->
-                    </div>
-                </div>
-                <!-- top navigation -->
 
-               
+                    <li><a href="<?php echo BASE_URL ?>admin/home/logout"><i class="fa fa-power-off"> </i> Logout</a>
+                    </li>
+
+
+                </ul>
+                </li>
+                </ul>
+            </div>
+
+        </div>
+        <!-- /sidebar menu -->
+
+        <!-- /menu footer buttons -->
+
+        <!-- /menu footer buttons -->
+    </div>
+</div>
+<!-- top navigation -->
