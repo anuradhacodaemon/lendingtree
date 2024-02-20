78ad2bfe5a9cacd71beda29f25f2675cbeb2a702
saurabh1390 <saurab.c@codaemonsoftwares.com>
2024-02-20 15:14:43 +0530

    demo portal

--- application/views/step9_view.php
+++ application/views/step9_view.php
@@ -42,9 +42,9 @@
                             </div>
                                 <div class="check">
                                 <div class="col-xs-12 col-sm-2 margbot_10">
-                                    <input type="checkbox" checked name="loan_insurance" value="loan_insurance"
+                                    <input type="checkbox" checked name="loan_insurance" value="loan_insurance" id="insurance"
                                     <?php if(isset($this->session->userdata['loan_insurance'])) { if($this->session->userdata['loan_insurance']== 'Y') echo 'checked="checked"'; else echo '';}  ?>
-                                    class="form-control width_100 button" id="loan_insurance" required>
+                                    class="form-control width_50 button" id="loan_insurance" required>
                                     <span id="err1" style="color: red"></span>
                                 </div>
                                 <div class="col-xs-12 col-sm-8 margbot_10">
@@ -63,7 +63,9 @@
         </div>
     </div>
 </div>
-
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 
 <script> //$('input[name=amount]').val()
  $(document).ready(function () {
@@ -78,9 +80,41 @@ if (!empty($this->session->flashdata('item'))) {
             $(".forgot_section").hide();
 <?php } ?>
     });
+
+    function insurance_vsc()
+{  
+   // $("#disclosureModal").modal('show'); // Open the modal
+ //  swal("Life Plus Protection helps protect the credit union and your family in the unforeseen event were you to suddenly pass away while still paying off this loan.\n In this scenario, your family would inherit a clean title to your vehicle without having to pay off your outstanding debt.");
+// Confirmation
+Swal.fire({
+  html: '<span class="swal-title">Life Plus Protection helps protect the credit union and your family in the unforeseen event were you to suddenly pass away while still paying off this loan<hr class="swal-hr--custom-color">In this scenario, your family would inherit a clean title to your vehicle without having to pay off your outstanding debt',
+})
+
+}
+
+    $(".insurance-sec").click(function(){
+        insurance_vsc();
+  });
 </script>
 <style>
  .insurance-sec {
     font-size: 20px;
   }
+  #insurance {
+  width: 30px;
+  height: 30px;
+}
+.swal-title {
+  font-size: 16px;
+}
+.swal2-popup {
+      width: 50%;
+ }
+.swal-hr--custom-color {
+  border-top: 1px solid #8a1b1e;
+}
+.swal2-styled.swal2-confirm {
+    font-size: 16px;
+}
+
 </style>
